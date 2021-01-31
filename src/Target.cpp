ee06c234439b174bf3bd5319bc6a3c679cce8e20
Giuliano <giulianoventuro1998@gmail.com>
2021-01-31 06:06:31 -0500

    add new target.png

--- src/Target.cpp
+++ src/Target.cpp
@@ -7,7 +7,7 @@
 
 Target::Target()
 {
-	TextureManager::Instance()->load("../Assets/textures/Circle.png","circle");
+	TextureManager::Instance()->load("../Assets/textures/coin.png","circle");
 
 	const auto size = TextureManager::Instance()->getTextureSize("circle");
 	setWidth(size.x);
